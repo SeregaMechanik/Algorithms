df0f2e16a5684f02a69e79a4e7472e10a4c4525d
SeregaMechanik <s-a.a3@mail.ru>
2025-02-04 02:04:23 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
-        Task2_RequestLogger requestLogger = new Task2_RequestLogger(10);
-        System.out.println(requestLogger.messageRequestDecision(1,"good morning"));
-        System.out.println(requestLogger.messageRequestDecision(5,"good morning"));
-        System.out.println(requestLogger.messageRequestDecision(9,"i need coffee"));
-        System.out.println(requestLogger.messageRequestDecision(10,"hello world"));
-        System.out.println(requestLogger.messageRequestDecision(11,"good morning"));
-        System.out.println(requestLogger.messageRequestDecision(15,"i need coffee"));
-        System.out.println(requestLogger.messageRequestDecision(17,"hello world"));
-        System.out.println(requestLogger.messageRequestDecision(25,"i need coffee"));
-        System.out.println(requestLogger.requests);
+        ArrayList<String> list = new ArrayList<String>() { };
+        list.add("0:start:0");
+        list.add("1:start:5");
+        list.add("2:start:8");
+        list.add("3:start:12");
+        list.add("4:start:15");
+        list.add("5:start:19");
+        list.add("5:end:22");
+        list.add("4:end:24");
+        list.add("3:end:27");
+        list.add("2:end:32");
+        list.add("1:end:35");
+        list.add("0:end:36");
+        int[] result = Task3_ExclusiveTime.exclusiveFunctionsTime(list, 6);
+        System.out.println(Arrays.toString(result));
     }
 
 }
